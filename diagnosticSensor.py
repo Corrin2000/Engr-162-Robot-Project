a1700005eda403c2ee8898189bbf7f98fcd684b7
Corrin2000 <enderrowe@hotmail.com>
2019-04-17 12:16:08 -0400

    added new file

---
+++ diagnosticSensor.py
@@ -0,0 +1,23 @@
+import time
+from module import BP, dT, sensors
+
+try:
+    time.sleep(2*dT)
+    while True:
+        dist_right = sensors.dataUltra[0]
+        dist_left = sensors.dataUltra[1]
+        dist_front = sensors.dataUltra[2]
+        IR_sqrt = sensors.dataIR
+        magnet_data = sensors.dataMag
+        gyro = sensors.dataGyro
+
+        print("front: %2d right: %2d left: %2d" % (dist_front, dist_right, dist_left))
+        print('gyro abs: %d' % (gyro[0]))
+        print('IR Sqrt: %d' % IR_sqrt)
+        print(magnet_data, '\n')
+        time.sleep(dT)
+except KeyboardInterrupt:
+    print('You pressed ctrl+c..')
+    BP.reset_all()
+finally:
+    sensors.stop()
